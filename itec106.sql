1b6206e3efd8dc3307539a3e5b74056d3e9bc322
Samantha Arabella Regencia <118825684+samantha-arabella-r@users.noreply.github.com>
2025-06-15 22:10:03 +0800

    Updated SQL file
    Dinelete ko yung dating file ng sql, pinalitan ko nung updated na may gawa ko

--- itec106.sql
+++ itec106.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2025 at 05:15 PM
+-- Generation Time: Jun 15, 2025 at 04:06 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `itec106`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `c_id` int(50) NOT NULL,
+  `c_fname` varchar(255) NOT NULL,
+  `c_mname` varchar(255) NOT NULL,
+  `c_lname` varchar(255) NOT NULL,
+  `c_street` varchar(255) NOT NULL,
+  `c_barangay` varchar(255) NOT NULL,
+  `c_city` varchar(255) NOT NULL,
+  `c_province` varchar(255) NOT NULL,
+  `c_postal` int(10) NOT NULL,
+  `c_contact` bigint(20) NOT NULL,
+  `date_created` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`c_id`, `c_fname`, `c_mname`, `c_lname`, `c_street`, `c_barangay`, `c_city`, `c_province`, `c_postal`, `c_contact`, `date_created`) VALUES
+(1, 'Liane', 'Adrielle', 'Amano', 'Carriedo', 'Pole', 'Imus', 'Cavite', 4107, 9458870432, '2025-06-15');
 
 -- --------------------------------------------------------
 
@@ -69,14 +96,17 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`id`, `position`, `username`, `fname`, `mname`, `lname`, `street`, `barangay`, `city`, `province`, `postal`, `contact`, `salary`, `password`, `confirm`) VALUES
-(1, 'admin', 'Original', 'Troy Aguiluz', 'Cads', 'Obligar', 'Blk 25 Lot 1 Richwood Townhomes', 'Navarro', 'General Trias', 'Cavite', 4107, 9270673497, 75000.00, 'admin@09', 'admin@09'),
-(2, 'staff', 'Overwork', 'John', 'Herman', 'Done', '59 San Antonio Street', 'SFDM', 'Quezon City', 'n/a', 1105, 9338151854, 24000.00, 'staff@09', 'staff@09'),
-(3, 'staff', 'Second', 'Joe', 'Neeman', 'Dane', 'Blk 1 Lot 25 Peninsula Homes', 'Navarro', 'General Trias', 'Cavite', 4107, 9352945986, 25500.50, 'staff@2nd', 'staff@2nd'),
-(4, 'staff', 'Joban', 'Jovhan', 'n/a', 'Amamangpang', 'Bisaya Street', 'Bisayawa', 'Davao', 'Mindanao', 8000, 9977609412, 100.00, 'bisakol@1', 'bisakol@1');
+(1, 'staff', 'sam06', 'Samantha Arabella', 'Redilla', 'Regencia', 'Jordan ', 'Bacao 1', 'General Trias', 'Cavite', 4107, 9198675122, 50000.00, 'Sam19.', 'Sam19.');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`c_id`);
 
 --
 -- Indexes for table `staff`
@@ -89,10 +119,16 @@ ALTER TABLE `staff`
 --
 
 --
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `c_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
